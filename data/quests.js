b6bfa0b8e3049c5e9fc96a44dbad9f9b8a2f1988
ninety-nine-luciverus <kamagalas13@gmail.com>
2025-05-29 17:18:34 +0700

    Create quests.js

---
+++ data/quests.js
@@ -0,0 +1,31 @@
+{
+  "starting_quest": {
+    "id": "starting_quest",
+    "name": "Mystery of the Shining Stone",
+    "description": "A villager asks you to find a lost 'shining stone' somewhere around the village. They believe it brings good fortune.",
+    "requirements": {
+      "item": "shining_stone",
+      "location": "starting_village"
+    },
+    "rewards": {
+      "exp": 50,
+      "item": "silver_coin"
+    },
+    "status": "active"
+  },
+  "fox_trail": {
+    "id": "fox_trail",
+    "name": "Trail of the Cunning Fox",
+    "description": "There are reports of a nine-tailed fox bothering travelers in the Dense Forest. Find out what it wants.",
+    "requirements": {
+      "creature_interaction": "nine_tailed_fox",
+      "location": "dense_forest",
+      "player_level": 3
+    },
+    "rewards": {
+      "exp": 150,
+      "item": "lucky_charm"
+    },
+    "status": "inactive"
+  }
+}
